170d9589ad15eda172c9aa099154666132a30a3a
Yukio Nozawa <personal@nyanchangames.com>
2025-01-25 15:35:02 +0900

    4拍子ごとにtickできる

--- src/logic/sound.js
+++ src/logic/sound.js
@@ -2,6 +2,7 @@ import { Howl } from "howler";
 
 const howlMap = {}
 let initialized = false
+let ticking = false;
 
 export function initSounds() {
   if(initialized) {
@@ -11,9 +12,24 @@ export function initSounds() {
     src: [process.env.PUBLIC_URL + "sounds/bgm1.wav"],
     loop: true,
   });
+  howlMap["music"].on("play", () => {
+    if(ticking) {
+      return;
+    }
+    setInterval(musicTick, 500)
+    ticking = true;
+  })
+  howlMap["beep"] = new Howl({
+    src: [process.env.PUBLIC_URL + "sounds/beep.wav"],
+  })
+
   initialized = true
 }
 
 export function loopMainMusic() {
   howlMap["music"].play()
+}
+
+function musicTick() {
+  howlMap["beep"].play()
 }
