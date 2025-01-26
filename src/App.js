3e45ed76b94f1d6ec7fc52f134f27f1ef819fbc1
KuchikiYusuke <kuchikiyusuke@yahoo.co.jp>
2025-01-26 15:16:48 +0900

    fix:typo

--- src/App.js
+++ src/App.js
@@ -44,36 +44,36 @@ function App() {
       {active === "opening" && <div className='opening-image-wrapper' onClick={handleStartButtonPress}>
         <img
           className='family-image'
-          src={process.env.PUBLIC_URL + "/art/titleUi.png"}
+          src={process.env.PUBLIC_URL + "/art/titleUI.png"}
           alt="ホライモリの家族 logo: オレンジ色の背景に、ゲームタイトルと、大きく口を開けたイモリのキャラクターが並ぶ。左上に紫色、左下に青緑色、中央に緑色、右に赤色。"
         />
         {/* <button onClick={handleStartButtonPress}>Game Start</button> */}
       </div>}
       {active === "active" && <>
         <div className='image-wrapper' onClick={() => handleBubbleButtonPress(setPhase)}>
-          <img className='bg-image' src={process.env.PUBLIC_URL + "art/background.png"}></img>
+          <img className='bg-image' src={process.env.PUBLIC_URL + "/art/background.png"}></img>
           {
             uraimoriIndex === 0 && <span className='character1'>
-            {isOpenMouse && <img className='character1-openmouse' src={process.env.PUBLIC_URL + "art/char1_frame2.png"}></img>}
-            {!isOpenMouse && <img className='character1-openmouse' src={process.env.PUBLIC_URL + "art/char1_frame1.png"}></img>}
+            {isOpenMouse && <img className='character1-openmouse' src={process.env.PUBLIC_URL + "/art/char1_frame2.png"}></img>}
+            {!isOpenMouse && <img className='character1-openmouse' src={process.env.PUBLIC_URL + "/art/char1_frame1.png"}></img>}
           </span>
           }
           {
             uraimoriIndex === 1 && <span className='character2'>
-            {isOpenMouse && <img className='character2-openmouse' src={process.env.PUBLIC_URL + "art/char2_frame2.png"}></img>}
-            {!isOpenMouse && <img className='character2-openmouse' src={process.env.PUBLIC_URL + "art/char2_frame1.png"}></img>}
+            {isOpenMouse && <img className='character2-openmouse' src={process.env.PUBLIC_URL + "/art/char2_frame2.png"}></img>}
+            {!isOpenMouse && <img className='character2-openmouse' src={process.env.PUBLIC_URL + "/art/char2_frame1.png"}></img>}
           </span>
           }
           {
             uraimoriIndex === 2 && <span className='character3'>
-            {isOpenMouse && <img className='character3-openmouse' src={process.env.PUBLIC_URL + "art/char3_frame2.png"}></img>}
-            {!isOpenMouse && <img className='character3-openmouse' src={process.env.PUBLIC_URL + "art/char3_frame1.png"}></img>}
+            {isOpenMouse && <img className='character3-openmouse' src={process.env.PUBLIC_URL + "/art/char3_frame2.png"}></img>}
+            {!isOpenMouse && <img className='character3-openmouse' src={process.env.PUBLIC_URL + "/art/char3_frame1.png"}></img>}
           </span>
           }
           {
             uraimoriIndex === 3 && <span className='character4'>
-            {isOpenMouse && <img className='character4-openmouse' src={process.env.PUBLIC_URL + "art/char4_frame2.png"}></img>}
-            {!isOpenMouse && <img className='character4-openmouse' src={process.env.PUBLIC_URL + "art/char4_frame1.png"}></img>}
+            {isOpenMouse && <img className='character4-openmouse' src={process.env.PUBLIC_URL + "/art/char4_frame2.png"}></img>}
+            {!isOpenMouse && <img className='character4-openmouse' src={process.env.PUBLIC_URL + "/art/char4_frame1.png"}></img>}
           </span>
           }
           <span className='mistake-number'>{mistakeNum}</span>
@@ -81,8 +81,8 @@ function App() {
       </>}
       {(active === "pause") && <>
         <div className='image-wrapper'  onClick={handlePlayButtonPress}>
-          <img className='bg-image' src={process.env.PUBLIC_URL + "art/background.png"}></img>
-          <img className='bg-image' src={process.env.PUBLIC_URL + "art/pause.png"}></img>
+          <img className='bg-image' src={process.env.PUBLIC_URL + "/art/background.png"}></img>
+          <img className='bg-image' src={process.env.PUBLIC_URL + "/art/pause.png"}></img>
           <span className='mistake-number'>{mistakeNum}</span>
         </div>
 
@@ -93,8 +93,8 @@ function App() {
       </>}
       {(active === "end") && <>
         <div className='image-wrapper'>
-          <img className='bg-image' src={process.env.PUBLIC_URL + "art/background.png"}></img>
-          <img className='bg-image' src={process.env.PUBLIC_URL + "art/endscreen.png"}></img>
+          <img className='bg-image' src={process.env.PUBLIC_URL + "/art/background.png"}></img>
+          <img className='bg-image' src={process.env.PUBLIC_URL + "/art/endscreen.png"}></img>
           <span className='mistake-number-ending'>{mistakeNum}</span>
         </div>
 
